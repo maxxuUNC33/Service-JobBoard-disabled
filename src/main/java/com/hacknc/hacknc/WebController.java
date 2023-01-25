22c2139852000c663e5c25bc4a66ef598410fc8d
Better Fork It <69515675+maxxuUNC33@users.noreply.github.com>
2023-01-25 01:57:31 -0500

    Update WebController.java

--- src/main/java/com/hacknc/hacknc/WebController.java
+++ src/main/java/com/hacknc/hacknc/WebController.java
@@ -5,27 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
-
     @GetMapping("/form")
     public String homePage() {
         return "form";
     }
-
     @GetMapping("/job_cards")
     public String jobCards() {
         return "job_cards";
     }
-
     @GetMapping("/g_auth")
     public String homepage() {
         return "g_auth";
     }
-
     @GetMapping("/job_list")
     public String jobList() {
         return "job_list";
     }
-
     @GetMapping("/offer")
     public String offerPage() {
         return "offer";
